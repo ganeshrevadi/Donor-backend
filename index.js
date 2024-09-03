b955a9ad1f931cd7fb07738ece055d34225b277f
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-09-03 23:12:47 +0530

    Email push notification

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 const { Form } = require("./db");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+// Configure your email transport
+const transporter = nodemailer.createTransport({
+  service: "gmail", // or any other email service
+  auth: {
+    user: "ganeshrevadi16@gmail.com", // Your email
+    pass: process.env.PASS, // Your email password or app password
+  },
+});
+
+app.get("/getuser/:id", async (req, res) => {
+  const number = req.params.id;
+  try {
+    const user = await Form.findOne({ number: number });
+
+    if (user) {
+      // Send email
+      const mailOptions = {
+        from: "ganeshrevadi16@gmail.com",
+        to: user.email, // Send email to the user
+        subject: "Keep food ready for pickup",
+        text: `Hello ${user.name} we are out for picking up the food. Please keep the food ready at ${user.location}. We will be there in 30 minutes.`,
+      };
+
+      await transporter.sendMail(mailOptions);
+
+      res.json({
+        message: "User details fetched and email sent successfully",
+        user: user,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 });
 
 app.post("/form", async (req, res) => {
