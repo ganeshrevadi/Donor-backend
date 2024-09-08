5536e83500c4587b434880c0eed229a5703d9710
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-09-08 16:03:41 +0530

    Push notification

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const { Form } = require("./db");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const nodemailer = require("nodemailer");
-
+// const nodemailer = require("nodemailer");
+const axios = require("axios");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Configure your email transport
-const transporter = nodemailer.createTransport({
-  service: "gmail", // or any other email service
-  auth: {
-    user: "ganeshrevadi16@gmail.com", // Your email
-    pass: process.env.PASS, // Your email password or app password
-  },
-});
 
 app.post("/getuser/:id", async (req, res) => {
   const number = req.params.id;
@@ -26,19 +17,30 @@ app.post("/getuser/:id", async (req, res) => {
     const user = await Form.findOne({ number: number });
 
     if (user) {
-      // Send email
-      const mailOptions = {
-        from: "ganeshrevadi16@gmail.com",
-        to: user.email, // Send email to the user
-        subject: "Keep food ready for pickup",
-        text: `Hello ${user.name} we are out for picking up the food. Please keep the food ready at ${user.location}. We will be there in 30 minutes.`,
-      };
+      const messageBody = `Hello ${user.name}, we are out for picking up the food. Please keep the food ready at ${user.location}. We will be there in 30 minutes.`;
 
-      await transporter.sendMail(mailOptions);
+      const API_TOKEN = process.env.API_TOKEN;
+      const SINCH_NUMBER = process.env.SINCH_NUMBER;
+      const TO_NUMBER = ["+91" + user.number];
+      const SINCH_URL = process.env.SINCH_URL;
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + API_TOKEN,
+      };
+      const payload = JSON.stringify({
+        from: SINCH_NUMBER,
+        to: TO_NUMBER,
+        body: messageBody,
+      });
+
+      await axios
+        .post(SINCH_URL, payload, { headers })
+        .then((response) => console.log(response.data))
+        .catch((error) => console.error("There was an error!", error.response));
 
       res.json({
-        message: "User details fetched and email sent successfully",
-        user: user,
+        message: "User details fetched and SMS sent successfully",
+        user: user.name,
       });
     } else {
       res.status(404).json({ message: "User not found" });
@@ -47,6 +49,34 @@ app.post("/getuser/:id", async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 });
+
+// app.post("/getuser/:id", async (req, res) => {
+//   const number = req.params.id;
+//   try {
+//     const user = await Form.findOne({ number: number });
+
+//     if (user) {
+//       // Send email
+//       const mailOptions = {
+//         from: "ganeshrevadi16@gmail.com",
+//         to: user.email, // Send email to the user
+//         subject: "Keep food ready for pickup",
+//         text: `Hello ${user.name} we are out for picking up the food. Please keep the food ready at ${user.location}. We will be there in 30 minutes.`,
+//       };
+
+//       await transporter.sendMail(mailOptions);
+
+//       res.json({
+//         message: "User details fetched and email sent successfully",
+//         user: user.name,
+//       });
+//     } else {
+//       res.status(404).json({ message: "User not found" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error", error: error.message });
+//   }
+// });
 
 app.post("/form", async (req, res) => {
   const name = req.body.name;
